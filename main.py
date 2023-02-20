5d2b96842ba6daab99d42e6bbb17b31415c196eb
abatuFF <nurbekuuludaniel@gmail.com>
2023-02-20 22:04:26 +0600

    added a couple of comments

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+#In this code, we use "win32com.client" to control Word through "COM (Component Object Model)"
 import win32com.client as win32
 import os
-
+#In the "convert_to_pdf" function, we open the file "file_path",
+# change the extension to ".pdf", save the document in the new format and close it.
+# Then we exit "Word"
 def convert_to_pdf(file_path):
     word = win32.Dispatch('Word.Application')
     doc = word.Documents.Open(file_path)
@@ -10,6 +13,6 @@ def convert_to_pdf(file_path):
     word.Quit()
 
 if __name__ == '__main__':
-    # source file path example
+    #enter here the path to the file to be converted
     file_path = r"D:\maindirectory\test.docx"
     convert_to_pdf(file_path)
